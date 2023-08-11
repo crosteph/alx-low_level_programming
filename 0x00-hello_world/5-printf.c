830a1e0f664c21b38627a4502c4558a4bbd42d4f
crosteph <funmisoule@gmail.com>
2023-08-10 18:52:43 -0700

    6-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A c program that prints using print-f
- * Return: 0 (Success)
- */
+*Description: main - Prints out a string to stdout.
+*Return: 0 if success.
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }	
